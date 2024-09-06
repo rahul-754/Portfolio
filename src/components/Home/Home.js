caa59da361dc948720be6f6d2a8d3098133e02d4
rahul-754 <161922950+rahul-754@users.noreply.github.com>
2024-09-06 18:18:33 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> RAHUL KUMAR</strong>
+                <strong className="main-name"> RAHUL PATEL</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
